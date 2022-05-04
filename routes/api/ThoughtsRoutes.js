080c68a0102b2edb9d05c4afca39623948a282d9
Spencer Kerans <skerans0@gmail.com>
2022-05-04 11:12:16 -0600

    finished thought controller, finished user/thoughts routes

--- routes/api/ThoughtsRoutes.js
+++ routes/api/ThoughtsRoutes.js
@@ -1,3 +1,20 @@
 const router = require('express').Router();
 
+const {
+getThoughts,
+getOneThought,
+addNewThought,
+updateThought,
+deleteThought,
+addNewReaction,
+deleteReaction
+} = require('../../controllers/thoughtsController');
+
+router.route('/').get(getThoughts).post(addNewThought);
+
+router.route('/:thoughtId').get(getOneThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addNewReaction).delete(deleteReaction);
+
+
 module.exports = router;
